e4e1d213295aa75c7a9639f160ba4bd06dff0477
Skyerin <44615808+skyerin8@users.noreply.github.com>
2019-12-19 22:38:09 +0000

    added aliases

--- commands/zucc.js
+++ commands/zucc.js
@@ -1,5 +1,6 @@
 module.exports = {
 	name: 'zucc',
+	aliases: ['duck', 'quack'],
 	description: 'flaming duck',
 	execute(message) {
 		message.channel.send('https://pbs.twimg.com/profile_images/823657569168203776/KNXvl1U-.jpg');
