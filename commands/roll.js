188d99f74d9ee2628f72012bce418f27648f95e1
Skyerin <44615808+skyerin8@users.noreply.github.com>
2019-12-19 20:16:45 +0000

    stopped rolls from being below 0. deafulted to 1d6 when the user types d6

--- commands/roll.js
+++ commands/roll.js
@@ -4,11 +4,20 @@ module.exports = {
 	execute(message, args) {
 		let str = args.toString();
 		let rolling = str.split('d');
-
+		let error = message.channel.send('please use the format [number of dice to roll]d[number of faces on the die]!');
 		let numToRoll = parseInt(rolling[0]);
 		let diceNum = rolling[1];
 		let total = 0;
 		let rolls = '';
+
+		if (numToRoll == undefined || diceNum === undefined){ 
+			return error;
+		}
+		if (numToRoll <= 0 || diceNum <=0){
+			return error;
+		}
+
+		if (isNaN(numToRoll)) {numToRoll = 1;}
 
 		for (let i = 0; i < numToRoll; i++){
 			var thisRoll = (Math.floor(Math.random() * diceNum)) + 1;
@@ -24,5 +33,6 @@ module.exports = {
 		}
 
 		message.channel.send(rolls + ' = ' + total);
+		
 	},
 }; 
