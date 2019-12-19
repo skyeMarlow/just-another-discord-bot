0df378b601b86a60d062aebda82fbe4f4468f6db
Skyerin <44615808+skyeMarlow@users.noreply.github.com>
2019-12-19 22:48:12 +0000

    delete accidental commit

--- stats.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = {
-	name: 'stats',
-	description: 'where is the bot right now?',
-	execute(message) {
-		message.channel.send( `${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
-	},
-}; 
-//this does not work yet S
--- zucc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	name: 'zucc',
-	aliases: ['duck', 'quack'],
-	description: 'flaming duck',
-	execute(message) {
-		message.channel.send('https://pbs.twimg.com/profile_images/823657569168203776/KNXvl1U-.jpg');
-	},
-}; 
