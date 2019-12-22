fd022d1ded2afc35e5c1174f8ab65b2e345854ab
Skyerin <44615808+skyerin8@users.noreply.github.com>
2019-12-22 16:55:09 +0000

    added music test as I do not have an API key

---
+++ commands/music.js
@@ -0,0 +1,21 @@
+const ytdl = require('ytdl-core');
+module.exports = {
+	name: 'music',
+	aliases: ['play'],
+	execute(message) {
+		if (message.channel.type !== 'text') return;
+
+		const { voiceChannel } = message.member;
+
+		if (!voiceChannel) {
+			return message.reply('please join a voice channel first!');
+		}
+
+		voiceChannel.join().then(connection => {
+			const stream = ytdl('https://www.youtube.com/watch?v=dQw4w9WgXcQ', { filter: 'audioonly' });
+			const dispatcher = connection.playStream(stream);
+
+			dispatcher.on('end', () => voiceChannel.leave());
+		});
+	},
+};
