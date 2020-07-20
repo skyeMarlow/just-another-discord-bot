b47a81346275f0d918c7dd563e422192671b8db6
Skyerin <44615808+skyerin8@users.noreply.github.com>
2020-07-20 14:29:42 +0100

    removed keepAlive() function

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ for (var file of commandFiles) {
 }
 
 client.on('ready', () => {
-	keepAlive();
     console.log(`Logged in as ${client.user.tag}!` + 'I am ready to go!');
     console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
     client.user.setActivity(`currently in ${client.guilds.size} servers`, { type: 'PLAYING' });
